368a29f2e7f9d25d1027dfbc00831281097598ab
DESKTOP-HO84V0D\hoya <icepega@gmail.com>
2021-10-15 15:47:48 +0900

    commit

--- src/main/java/com/cos/person/config/BindingAdvice.java
+++ src/main/java/com/cos/person/config/BindingAdvice.java
@@ -1,10 +1,20 @@
 package com.cos.person.config;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
 public class BindingAdvice {
+	
+	
+	//@Before
+	//@After
+	@Around("execution(* com.cos.person.web..*Controller.*(..))")
+	public void validCheck(ProceedingJoinPoint proceedingJoinPoint) {
+		
+	}
 
 }
